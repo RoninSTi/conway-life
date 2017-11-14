6637c848e7bd66f3f275a48615a1db21d6311287
Craig Cronin <craig.cronin@gmail.com>
2017-11-14 17:54:06 -0500

    Style

--- src/Utils/LifeHelper.js
+++ src/Utils/LifeHelper.js
@@ -2,6 +2,12 @@ import store from '../Store';
 
 function getNeighbors(cell) {
   const state = store.getState();
+
+  /*
+  * Setup the continuous grid.
+  * The col to the left of the left-most col is the col furthest to the right, etc
+  */
+  
   let rowAbove = cell.row - 1 < 0 ? state.life.grid.rows - 1:cell.row - 1;
   let rowBelow = cell.row + 1 === state.life.grid.rows ? 0:cell.row + 1;
   let colLeft = cell.col - 1 < 0 ? state.life.grid.cols - 1:cell.col - 1;
@@ -9,38 +15,39 @@ function getNeighbors(cell) {
 
   let neighbors = [];
 
-  neighbors.push({
-    row: cell.row,
-    col: colLeft
-  });
-  neighbors.push({
-    row: rowAbove,
-    col: colLeft
-  });
-  neighbors.push({
-    row: rowAbove,
-    col: cell.col
-  });
-  neighbors.push({
-    row: rowAbove,
-    col: colRight
-  });
-  neighbors.push({
-    row: cell.row,
-    col: colRight
-  });
-  neighbors.push({
-    row: rowBelow,
-    col: colRight
-  });
-  neighbors.push({
-    row: rowBelow,
-    col: cell.col
-  });
-  neighbors.push({
-    row: rowBelow,
-    col: colLeft
-  });
+  neighbors
+    .push({
+      row: cell.row,
+      col: colLeft
+    })
+    .push({
+      row: rowAbove,
+      col: colLeft 
+    })
+    .push({
+      row: rowAbove,
+      col: cell.col
+    })
+    .push({
+      row: rowAbove,
+      col: colRight
+    })
+    .push({
+      row: cell.row,
+      col: colRight
+    })
+    .push({
+      row: rowBelow,
+      col: colRight
+    })
+    .push({
+      row: rowBelow,
+      col: cell.col
+    })
+    .push({
+      row: rowBelow,
+      col: colLeft
+    });
 
   return neighbors;
 }
