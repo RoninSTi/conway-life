e699a9c79d4d46ed6661a0eb2c6aca04e71632fa
Craig Cronin <craig.cronin@gmail.com>
2017-11-15 00:41:56 -0500

    Fix array

--- src/Utils/LifeHelper.js
+++ src/Utils/LifeHelper.js
@@ -7,47 +7,46 @@ function getNeighbors(cell) {
   * Setup the continuous grid.
   * The col to the left of the left-most col is the col furthest to the right, etc
   */
-  
-  let rowAbove = cell.row - 1 < 0 ? state.life.grid.rows - 1:cell.row - 1;
-  let rowBelow = cell.row + 1 === state.life.grid.rows ? 0:cell.row + 1;
-  let colLeft = cell.col - 1 < 0 ? state.life.grid.cols - 1:cell.col - 1;
-  let colRight = cell.col + 1 === state.life.grid.cols ? 0:cell.col + 1;
 
-  let neighbors = [];
+  const rowAbove = cell.row - 1 < 0 ? state.life.grid.rows - 1:cell.row - 1;
+  const rowBelow = cell.row + 1 === state.life.grid.rows ? 0:cell.row + 1;
+  const colLeft = cell.col - 1 < 0 ? state.life.grid.cols - 1:cell.col - 1;
+  const colRight = cell.col + 1 === state.life.grid.cols ? 0:cell.col + 1;
 
-  neighbors
-    .push({
+  const neighbors = [
+    {
       row: cell.row,
       col: colLeft
-    })
-    .push({
+    },
+    {
       row: rowAbove,
       col: colLeft 
-    })
-    .push({
+    },
+    {
       row: rowAbove,
       col: cell.col
-    })
-    .push({
+    },
+    {
       row: rowAbove,
       col: colRight
-    })
-    .push({
+    },
+    {
       row: cell.row,
       col: colRight
-    })
-    .push({
+    },
+    {
       row: rowBelow,
       col: colRight
-    })
-    .push({
+    },
+    {
       row: rowBelow,
       col: cell.col
-    })
-    .push({
+    },
+    {
       row: rowBelow,
       col: colLeft
-    });
+    }
+  ];
 
   return neighbors;
 }
